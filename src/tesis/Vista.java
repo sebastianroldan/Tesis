f0d19f1f2901baf0088fea0cc51e4f6429b6e7e1
sebastianroldan <rolsebastian@gmail.com>
2020-08-24 10:41:39 -0300

    Se agrega la clase Vista

---
+++ src/tesis/Vista.java
@@ -0,0 +1,13 @@
+package tesis;
+
+public class Vista {
+
+	private String titulo;
+	
+	public void mostrarTitulo(String nuevoTitulo) {
+		
+		this.titulo = nuevoTitulo;
+		System.out.print(this.titulo);
+		
+	}
+}
